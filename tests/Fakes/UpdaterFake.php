9dd3e29601c1682576c901ba4766ab28e36ffd45
distinctm <jani@marketdistinctly.com>
2019-08-23 09:53:05 -0400

    Rename

---
+++ tests/Fakes/UpdaterFake.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace distinctm\LaravelDataSync\Tests\Fakes;
+
+use distinctm\LaravelDataSync\Updater;
+
+class UpdaterFake extends Updater
+{
+    protected function getModel(string $name)
+    {
+        return '\\distinctm\\LaravelDataSync\\Tests\\'.studly_case(
+            pathinfo($name, PATHINFO_FILENAME)
+        );
+    }
+}
