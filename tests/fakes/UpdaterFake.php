5f7fa8491420a382a362c6fc2e5d43e5b956c58b
nullthoughts <jani@nullincorporated.com>
2019-10-15 12:29:30 -0400

    Manually merge PR 16 from @vicgonvt

--- tests/fakes/UpdaterFake.php
+++ tests/fakes/UpdaterFake.php
@@ -1,15 +1,16 @@
 <?php
 
-namespace distinctm\LaravelDataSync\Tests\fakes;
+namespace distinctm\LaravelDataSync\Tests\Fakes;
 
 use distinctm\LaravelDataSync\Updater;
+use Illuminate\Support\Str;
 
 class UpdaterFake extends Updater
 {
     protected function getModel(string $name)
     {
-        return '\\distinctm\\LaravelDataSync\\Tests\\'.studly_case(
-            pathinfo($name, PATHINFO_FILENAME)
-        );
+        return '\\distinctm\\LaravelDataSync\\Tests\\'.Str::studly(
+                pathinfo($name, PATHINFO_FILENAME)
+            );
     }
 }
